f2fe4b32b6ac815ebf86b80c9e3c777db1acf9b5
Muhammad Adeel <adeel@dhis2.org>
2018-03-14 11:47:25 +0100

    style(Project): add GitHub external link to Project

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -17,6 +17,16 @@ import saFlag from 'assets/images/flags/sa.png'
 import usFlag from 'assets/images/flags/us.png'
 
 import { Topic } from './Projects'
+
+const Icon = styled.a`
+  font-size: 20px;
+  color: #000;
+
+  &:hover {
+    text-decoration: none;
+  }
+`
+
 const TopicContainer = styled.span`
   margin-right: 10px;
 `
@@ -136,8 +146,23 @@ class ProjectPage extends Page {
     return (
       <Template>
         <div className="mt-4">
-          <h3 className="mb-3">{repo.name}</h3>
-          <p className="font-italic mt-3">{repo.description}</p>
+          <div className="row">
+            <div className="col">
+              <div className="d-flex d-100 justify-content-between">
+                <h3 className="mb-3">{repo.name}</h3>
+                <div>
+                  <Icon
+                    className="fa fa-github"
+                    href={`https://github.com/${owner}/${repoName}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  />
+                </div>
+              </div>
+
+              <p className="font-italic mt-3">{repo.description}</p>
+            </div>
+          </div>
 
           <div className="mt-2">
             <Topics list={repo.topics} />
